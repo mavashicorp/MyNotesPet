e2783cc6a7268444441cc5a38f1b85a6718f20e1
Dmitriy <mavashicorp@gmail.com>
2024-05-10 16:25:46 +0300

    wrote NotesDBContext

---
+++ MyNotesPet/DataAccess/NotesDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyNotesPet.DataAccess
+{
+    public class NotesDBContext: DbContext
+    {
+        private readonly IConfiguration _configuration;
+        public NotesDBContext(IConfiguration configuration)
+        {
+            _configuration= configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(_configuration.GetConnectionString("Database"));
+            //подключает провайдер npgsql и передаю строку подключения
+        }
+
+    }
+
+   
+}
