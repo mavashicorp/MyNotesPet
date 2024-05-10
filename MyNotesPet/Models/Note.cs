8da77668b381a8911a2524028abba62049c9ba3f
Dmitriy <mavashicorp@gmail.com>
2024-05-10 16:04:54 +0300

    update Note.cs

--- MyNotesPet/Models/Note.cs
+++ MyNotesPet/Models/Note.cs
@@ -2,5 +2,17 @@
 {
     public class Note
     {
+        public Note(string title, string description)
+        {
+            Title = title; 
+            Description = description;
+            CreatedAt = DateTime.Now; // по умолчанию дата создания
+        }
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
     }
 }
+
+//string.Empty - по умолчанию пустая строка
