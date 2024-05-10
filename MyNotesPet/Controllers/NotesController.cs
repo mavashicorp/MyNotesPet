ff7020c19c8758f1c1ecddbe2aff9c447d153de9
Dmitriy <mavashicorp@gmail.com>
2024-05-10 15:52:42 +0300

    add method in controlled  - Create and Get

--- MyNotesPet/Controllers/NotesController.cs
+++ MyNotesPet/Controllers/NotesController.cs
@@ -3,9 +3,20 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MyNotesPet.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class NotesController : ControllerBase
     {
+        [HttpPost]
+        public async Task<IActionResult> Create()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
     }
 }
